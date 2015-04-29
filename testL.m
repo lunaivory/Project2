79018b412ee194a866cde46c659220e487eb8d07
lunaivory <catlily234@gmail.com>
2015-04-30 02:24:45 +0800

    test with MATLAB default feature matching

---
+++ testL.m
@@ -0,0 +1,12 @@
+I1 = rgb2gray(imread('imgs/1.jpg'));
+I2 = rgb2gray(imread('imgs/2.jpg'));
+points1 = detectHarrisFeatures(I1);
+points2 = detectHarrisFeatures(I2);
+[features1, valid_points1] = extractFeatures(I1, points1);
+[features2, valid_points2] = extractFeatures(I2, points2);
+indexPairs = matchFeatures(features1, features2);
+matchedPoints1 = valid_points1(indexPairs(:, 1), :);
+matchedPoints2 = valid_points2(indexPairs(:, 2), :);
+figure;
+showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
+ransac(floor(matchedPoints1.Location), floor(matchedPoints2.Location), I1, I2);
