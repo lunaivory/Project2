e7e8b7f7ba939fbb63df94233fc0fce987d0e363
lunaivory <catlily234@gmail.com>
2015-04-30 02:09:29 +0800

    ransac done

--- ransac.m
+++ ransac.m
@@ -1,23 +1,34 @@
 function M = ransac(X1, X2, IMG1, IMG2)
   k = 100;
   c = 0;
-  d = 100;
-  n = 20;
+  d = 1;
   sz = size(X1, 1);
-
+  n = 4;
+  X11 = ones(sz, 3);
+  X11(:,1:2) = X1(:,1:2);
+  
   for i = 1:k
-    A = zeros(n, 2);
+    A = zeros(n, 3);
     B = zeros(n, 2);
     for j = 1:n
       itr = ceil(rand*sz);
-      A(j, :) = X1(itr, :);
+      A(j, :) = [X1(itr, :),1];
       B(j, :) = X2(itr, :);
     end
-    X = B\A;
-    nX1 = X1*X;
+    X = A\B;
+    disp(X);
+    nX1 = X11*X;
     D2 = (nX1-X2).*(nX1-X2);
     D = D2(:,1) + D2(:,2);
+    cal = 0;
+    for kk = 1:size(D)
+       fprintf('%d %d\n', nX1(kk,1)-X2(kk,1), nX1(kk,2)-X2(kk,2));
+       %fprintf('%d : %d < %d ? %d',kk, D(kk), d, D(kk) < d);
+       cal = cal + 1;
+    end
     tmp = sum(D < d);
+    %fprintf('%d vs %d', cal, tmp);
+    disp(tmp);
     if tmp > c
       c = tmp;
       M = X;
@@ -41,7 +52,7 @@ function PlotRansac(X1, X2, D, IMG1, IMG2)
       %fprintf('No MATCH %d %d %d %d\n', x1, y1, x2, y2);
       plot(x1, y1, 'r.');
       plot(x2, y2, 'r.');
-      %line([x1 x2], [y1 y2], 'Color', 'w');
+      line([x1 x2], [y1 y2], 'Color', 'w');
     else
       %fprintf('MATCH %d %d %d %d\n', x1, y1, x2, y2);
       plot(x1, y1, 'go');
