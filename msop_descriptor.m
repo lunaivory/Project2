1a97911b971deefa9ed839ca6ffbbbb11c7839fc
Victor Hung <vhung@kth.se>
2015-04-30 23:36:09 +0800

    Added comments to msop_descriptor

--- msop_descriptor.m
+++ msop_descriptor.m
@@ -1,5 +1,6 @@
 function descriptors = msop_descriptor( img, points )
 
+    % Convert to grayscale if needed
     if(ndims(img) == 3)
         grayscale = im2double(rgb2gray(img));
     else
@@ -13,16 +14,22 @@ function descriptors = msop_descriptor( img, points )
         x = points(i, 1);
         y = points(i, 2);
         
+        % extract the patch around keypoint
         image_patch = grayscale((y-patch_size+1):(y+patch_size), ...
             (x-patch_size+1):(x+patch_size));
         
+        % down size the patch
         down = imresize(image_patch, 0.2);
         
+        % normalize intensity values
         oldMin = double(min(min(down(:,:))));
         oldMax = double(max(max(down(:,:))));
         down = double(down - oldMin) ./ (oldMax - oldMin);
+        
+        % reshape matrix into vector
         down = reshape(down, [1, descriptor_size]);
         
+        % save the result
         descriptors(i, :) = down;
     end
 end
