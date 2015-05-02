b19aa12665be2ecddbd3dbb20b1cb8e83e8b85b8
lunaivory <catlily234@gmail.com>
2015-05-02 15:48:41 +0800

    cylinder warp added

--- load_imgs.m
+++ load_imgs.m
@@ -1,15 +1,8 @@
 function [imgs, grayscales] = load_imgs( )
-    img1 = imread('photo/1.jpg');
-    img2 = imread('photo/2.jpg');
-    img3 = imread('photo/3.jpg');
-    img4 = imread('photo/4.jpg');
-    img5 = imread('photo/5.jpg');
-    img6 = imread('photo/6.jpg');
-    img7 = imread('photo/7.jpg');
-    
-    imgs = {img1, img2, img3, img4, img5, img6, img7};
-    grayscales = {rgb2gray(img1), rgb2gray(img2), rgb2gray(img3)...
-        rgb2gray(img4), rgb2gray(img5), rgb2gray(img6), rgb2gray(img7)};
-    
+    for i = 1:7
+        filename = sprintf('./photo/%d.jpg', i);
+        imgs{i} = cyl(imread(filename), 1800);
+        grayscales{i} = rgb2gray(imgs{i});
+    end  
 end
 
